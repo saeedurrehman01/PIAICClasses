6cce2ab9008b1209fbb47f842b2c9b56427665a7
saeedurrehman01 <saeedrehman241@gmail.com>
2023-09-16 01:23:55 +0500

    Class 10 completed

---
+++ Class 10/User Input/calculator.js
@@ -0,0 +1,42 @@
+import inquirer from "inquirer";
+const questions = [
+    {
+        name: "num1",
+        type: "number",
+        message: "What is your Num1?: ",
+    },
+    {
+        name: "num2",
+        type: "input",
+        message: "What is your Num2?: ",
+    },
+    {
+        name: "Operations",
+        type: "list",
+        message: "Choose Operation: ",
+        choices: ["+", "-", "*", "/", "^2"],
+    },
+];
+var answers = inquirer.prompt(questions);
+answers
+    .then((answers) => {
+    switch (answers.Operations) {
+        case "*":
+            console.log(`${answers.num1} * ${answers.num2} = ${answers.num1 * answers.num2}`);
+            break;
+        case "+":
+            console.log(`${answers.num1} + ${answers.num2} = ${answers.num1 + answers.num2}`);
+        case "-":
+            console.log(`${answers.num1} - ${answers.num2} = ${answers.num1 - answers.num2}`);
+        case "/":
+            console.log(`${answers.num1} / ${answers.num2} = ${answers.num1 / answers.num2}`);
+        case "^2":
+            console.log(`${answers.num1} ^2 = ${answers.num1 * answers.num1}`);
+        default:
+            break;
+    }
+    // console.log(answers);
+})
+    .catch((err) => {
+    console.log("Something is wrong.");
+});
